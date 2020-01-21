d1f5b0fb66aaee5d36d8d1c51651d5088a4759c6
ReillyWolfe <41017065+ReillyWolfe@users.noreply.github.com>
2020-01-21 14:04:13 -0800

    Add files via upload

---
+++ lab0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# ###############################################################
+# #
+# Reilly Wolfe #
+# ECE 351 - 51 #
+# Lab 0 #
+# Due 1/28/2020 #
+#  #
+# #
+# ###############################################################
+
+import numpy
+import matplotlib . pyplot
+import scipy
+import control
+import pandas
